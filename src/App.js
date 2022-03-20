98c20b3e98a6966c4207913963483a5028983058
andrespty <andresholee@gmail.com>
2022-03-20 15:09:05 -0400

    Final code for video 5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+
+  const [ counter, setCounter ] = useState(0)
+
+  const handle_click = () => {
+    setCounter(counter + 5)
+  }
 
   return (
     <div className="App" >
 
+    <h1>Times button has been clicked:</h1>
+    <h1>{counter}</h1>
 
+    <button onClick={handle_click} >
+      Click me
+    </button>
 
     </div>
   );
